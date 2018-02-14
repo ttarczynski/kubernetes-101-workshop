d95927a9a1f40848f8bbc12f9c614e392d6174e5
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2018-02-14 12:38:06 +0100

    ch7: 	modified:   07-01_the_service_object.sh 	new file:   07-01_the_service_object_readinessProbe_patch.json

--- manifests/07-01_the_service_object.sh
+++ manifests/07-01_the_service_object.sh
@@ -7,7 +7,13 @@ kubectl expose deployment alpaca-prod
 kubectl run bandicoot-prod    --image=gcr.io/kuar-demo/kuard-amd64:2 --replicas=2 --port=8080 --labels="ver=2,app=bandicoot,env=prod"
 kubectl expose deployment bandicoot-prod
 
-kubectl get services -o wide
+# kubectl get services -o wide
+kubectl get services,deployments,rs,pods -owide --show-labels
 
+kubectl get endpoints alpaca-prod --watch &
+sleep 20
+kubectl patch deployment alpaca-prod -p "$(cat 07-01_the_service_object_readinessProbe_patch.json)"
+
+sleep 30
 ALPACA_POD=$(kubectl get pods -l app=alpaca -o jsonpath='{.items[0].metadata.name}')
 kubectl port-forward $ALPACA_POD 48858:8080
