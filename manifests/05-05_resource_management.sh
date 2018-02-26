aa60c7eb8cab3ef1fda7aa12d23ee4944a04047e
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2018-02-26 19:22:33 +0100

    ch5: 	modified:   05-05_resource_management.sh 	new file:   05-06_kuard-pod-reslim_more_pods.yaml 	new file:   05-06_resource_limits.sh

--- manifests/05-05_resource_management.sh
+++ manifests/05-05_resource_management.sh
@@ -34,10 +34,11 @@ read -p "Continue?"
 kubectl apply -f 05-05_kuard-pod-reqreq_b.yaml
 read -p "Continue?"
 kubectl get pod -o wide
+read -p "Continue?"
 kubectl describe pod kuard-b
 read -p "Continue?"
 
-# kuard-b: 0.5 CPU / 128 Mi Mem
+# kuard-c: 0.5 CPU / 128 Mi Mem
 diff 05-05_kuard-pod-reqreq.yaml 05-05_kuard-pod-reqreq_c.yaml
 read -p "Continue?"
 kubectl apply -f 05-05_kuard-pod-reqreq_c.yaml
@@ -48,9 +49,6 @@ KUARD_C_NODE=$(kubectl  get pod kuard-c -o jsonpath --template={.spec.nodeName})
 kubectl describe node $KUARD_C_NODE
 read -p "Continue?"
 
-# delete the kuard pod
-kubectl delete -f 05-05_*.yaml
+# delete all kuard pods
+kubectl delete pod --all
 read -p "Continue?"
-
-# cleenup
-kill $(jobs -p)
---
+++ manifests/05-06_resource_limits.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -x
+
+# what's changed in the kuard pod manifest:
+diff 05-05_kuard-pod-reqreq.yaml 05-06_kuard-pod-reslim.yaml
+read -p "Continue?"
+
+# apply the pod manifest
+kubectl apply -f 05-06_kuard-pod-reslim.yaml
+sleep 5
+read -p "Continue?"
+
+# observe effects of resources request
+kubectl describe pod kuard
+read -p "Continue?"
+
+# port forward
+kubectl port-forward kuard 8001:8080 &
+sleep 2
+echo "Open in a browser: http://127.0.0.1:8001/"
+read -p "Continue?"
+
+# allocate 500 MiB RAM to trigger OOM Killer
+echo "Go to: http://127.0.0.1:8001/-/mem and click: 'Allocate 500 MiB'"
+read -p "Continue?"
+
+# check if the pod was restarted
+kubectl get pod kuard
+read -p "Continue?"
+kubectl describe pod kuard
+read -p "Continue?"
+
+# deploy 3 more pods
+## kuard-a – CPU request: 0.5 / CPU limit: 1.0
+## kuard-b – CPU request: 0.5 / CPU limit: 1.0
+## kuard-c – CPU request: 0.5 / CPU limit: 1.0
+vim 05-06_kuard-pod-reslim_more_pods.yaml
+read -p "Continue?"
+kubectl apply -f 05-06_kuard-pod-reslim_more_pods.yaml
+read -p "Continue?"
+
+# observe how overcommit works
+kubectl get pods -o wide
+read -p "Continue?"
+kubectl describe node ks103
+read -p "Continue?"
+
+# delete all kuard pods
+kubectl delete pod --all
+read -p "Continue?"
+
+# cleenup
+kill $(jobs -p)
