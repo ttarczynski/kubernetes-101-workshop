2bd65eb77dddd672cc93279f6857deae018583ea
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2018-02-26 18:31:35 +0100

    ch5: 	renamed:    05-01_creating_a_pod.yaml -> 05-01_creating_a_pod.sh 	renamed:    05-02_running_pods.yaml -> 05-02_running_pods.sh 	renamed:    05-03_accessing_your_pod.yaml -> 05-03_accessing_your_pod.sh 	renamed:    05-04_health_checks.yaml -> 05-04_health_checks.sh 	renamed:    05-03_kuard-pod-reqreq.yaml -> 05-05_kuard-pod-reqreq.yaml 	renamed:    05-03_kuard-pod-reqreq_a.yaml -> 05-05_kuard-pod-reqreq_a.yaml 	renamed:    05-03_kuard-pod-reqreq_b.yaml -> 05-05_kuard-pod-reqreq_b.yaml 	renamed:    05-03_kuard-pod-reqreq_c.yaml -> 05-05_kuard-pod-reqreq_c.yaml 	new file:   05-05_resource_management.sh 	renamed:    05-04_kuard-pod-reslim.yaml -> 05-06_kuard-pod-reslim.yaml 	renamed:    05-05_kuard-pod-vol.yaml -> 05-07_kuard-pod-vol.yaml 	renamed:    05-06-kuard-pod-full.yaml -> 05-08-kuard-pod-full.yaml

---
+++ manifests/05-05_resource_management.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -x
+
+# what's changed in the kuard pod manifest:
+diff 05-04_kuard-pod-health.yaml 05-05_kuard-pod-reqreq.yaml
+read -p "Continue?"
+
+# apply the pod manifest
+kubectl apply -f 05-05_kuard-pod-reqreq.yaml
+read -p "Continue?"
+
+# observe effects of resources request
+kubectl describe pod kuard
+read -p "Continue?"
+
+KUARD_NODE=$(kubectl  get pod kuard -o jsonpath --template={.spec.nodeName})
+kubectl describe node $KUARD_NODE
+read -p "Continue?"
+
+# schedule more pods
+# kuard-a: 1.0 CPU / 128 Mi Mem
+diff 05-05_kuard-pod-reqreq.yaml 05-05_kuard-pod-reqreq_a.yaml
+read -p "Continue?"
+kubectl apply -f 05-05_kuard-pod-reqreq_a.yaml
+read -p "Continue?"
+KUARD_A_NODE=$(kubectl  get pod kuard-a -o jsonpath --template={.spec.nodeName})
+kubectl describe node $KUARD_A_NODE
+read -p "Continue?"
+
+# kuard-b: 1.0 CPU / 128 Mi Mem
+diff 05-05_kuard-pod-reqreq.yaml 05-05_kuard-pod-reqreq_b.yaml
+read -p "Continue?"
+kubectl apply -f 05-05_kuard-pod-reqreq_b.yaml
+read -p "Continue?"
+kubectl get pod -o wide
+kubectl describe pod kuard-b
+read -p "Continue?"
+
+# kuard-b: 0.5 CPU / 128 Mi Mem
+diff 05-05_kuard-pod-reqreq.yaml 05-05_kuard-pod-reqreq_c.yaml
+read -p "Continue?"
+kubectl apply -f 05-05_kuard-pod-reqreq_c.yaml
+read -p "Continue?"
+kubectl get pod -o wide
+read -p "Continue?"
+KUARD_C_NODE=$(kubectl  get pod kuard-c -o jsonpath --template={.spec.nodeName})
+kubectl describe node $KUARD_C_NODE
+read -p "Continue?"
+
+# delete the kuard pod
+kubectl delete -f 05-05_*.yaml
+read -p "Continue?"
+
+# cleenup
+kill $(jobs -p)
