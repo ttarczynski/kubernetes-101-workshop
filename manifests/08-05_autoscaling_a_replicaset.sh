ad7cc29e5e1eca548abccbd29104eef0bc6a5714
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2018-03-01 08:01:23 +0100

    ch8: 	modified:   manifests/08-05_autoscaling_a_replicaset.sh

--- manifests/08-05_autoscaling_a_replicaset.sh
+++ manifests/08-05_autoscaling_a_replicaset.sh
@@ -2,27 +2,34 @@
 
 set -x
 
+# view the pod manifest
+vim 08-05_autoscaling_a_replicaset_request.yaml
+read -p "Continue?"
+
 # set requested resources
 kubectl apply -f 08-05_autoscaling_a_replicaset_request.yaml
+read -p "Continue?"
+
+# display the rs
+kubectl get rs kuard -o wide
+read -p "Continue?"
 
 # anable hpa
 kubectl autoscale rs kuard --min=1 --max=5 --cpu-percent=80
+read -p "Continue?"
 
 # Observe HPA:
 kubectl get hpa -w &
 sleep 120
-
 read -p "Continue?"
 
 kubectl describe hpa kuard
-
 read -p "Continue?"
 
 # observe autoscaling:
 
 kubectl proxy &
 echo "Open grafana via: http://localhost:8001/api/v1/proxy/namespaces/kube-system/services/monitoring-grafana:80/?orgId=1"
-
 read -p "Continue?"
 
 ## generate load
@@ -33,10 +40,10 @@ read -p "Continue?"
 
 echo "Open in browser: http://localhost:48858/-/keygen"
 echo "And generate some CPU load"
-
 read -p "Continue?"
 
 kubectl describe hpa kuard
+read -p "Continue?"
 
-jobs
+kill $(jobs -p)
 read -p "Exit?"
