d7e5acd7fa4da60b8a3237d808edcb843a4d41a8
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2018-03-02 14:50:37 +0100

    ch08: fix resources for metrics-server, heapster, influxdb 	modified:   08-04_autoscaling_a_replicaset_install_heapster.sh 	new file:   08-04_resources_heapster_patch.yaml 	new file:   08-04_resources_metrics-server_patch.yaml 	new file:   08-04_resources_monitoring-influxdb_patch.yaml 	modified:   08-05_autoscaling_a_replicaset_request.yaml

--- manifests/08-04_autoscaling_a_replicaset_install_heapster.sh
+++ manifests/08-04_autoscaling_a_replicaset_install_heapster.sh
@@ -6,6 +6,7 @@ set -x
 #  https://github.com/kubernetes/heapster/blob/v1.5.1/docs/influxdb.md
 kubectl create -f heapster/deploy/kube-config/influxdb/
 kubectl create -f heapster/deploy/kube-config/rbac/heapster-rbac.yaml
+read -p "Continue?"
 
 # verify installation
 
@@ -20,3 +21,10 @@ kubectl top nodes
 #  https://kubernetes.io/docs/tasks/debug-application-cluster/core-metrics-pipeline/
 
 kubectl create -f metrics-server/deploy/
+read -p "Continue?"
+
+# apply some requests and limits on resources
+kubectl -n kube-system patch deployment heapster -p "$(cat 08-04_resources_heapster_patch.yaml)"
+kubectl -n kube-system patch deployment metrics-server -p "$(cat 08-04_resources_metrics-server_patch.yaml)"
+kubectl -n kube-system patch deployment monitoring-influxdb -p "$(cat 08-04_resources_monitoring-influxdb_patch.yaml)"
+read -p "Continue?"
