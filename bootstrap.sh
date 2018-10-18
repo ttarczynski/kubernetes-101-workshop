0ae3850eaccbe6f0315887cdd6fb5c005ee84fdd
nosal_rafa <rafal.nosal@gigaset.com>
2018-10-18 12:31:19 +0200

    modified:   bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -55,11 +55,11 @@ systemctl enable docker && systemctl start docker
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+baseurl=http://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
 enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+#gpgcheck=1
+#repo_gpgcheck=1
+#gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
 setenforce 0
 yum install -y kubelet kubeadm kubectl
