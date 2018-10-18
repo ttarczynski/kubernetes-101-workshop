da819358f585f00ba1d2550911e830b502b05425
nosal_rafa <rafal.nosal@gigaset.com>
2018-10-18 12:48:04 +0200

    modified:   bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -57,9 +57,9 @@ cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 name=Kubernetes
 baseurl=http://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
 enabled=1
-#gpgcheck=1
-#repo_gpgcheck=1
-#gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+gpgcheck=0
+repo_gpgcheck=0
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
 setenforce 0
 yum install -y kubelet kubeadm kubectl
