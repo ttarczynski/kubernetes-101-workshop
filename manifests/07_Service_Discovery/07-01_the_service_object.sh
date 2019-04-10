bc9c1c33a9a8fdeab8898d47728d556088982f94
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2019-04-10 18:11:21 +0200

    Corrections in 07

--- manifests/07_Service_Discovery/07-01_the_service_object.sh
+++ manifests/07_Service_Discovery/07-01_the_service_object.sh
@@ -4,18 +4,25 @@ set -x
 
 kubectl run alpaca-prod       --image=gcr.io/kuar-demo/kuard-amd64:1 --replicas=3 --port=8080 --labels="ver=1,app=alpaca,env=prod"
 kubectl expose deployment alpaca-prod
+read -p "Continue?"
+
 kubectl run bandicoot-prod    --image=gcr.io/kuar-demo/kuard-amd64:2 --replicas=2 --port=8080 --labels="ver=2,app=bandicoot,env=prod"
 kubectl expose deployment bandicoot-prod
-
 read -p "Continue?"
 
 # kubectl get services -o wide
-kubectl get services,deployments,rs,pods -owide --show-labels
+kubectl get services -owide --show-labels
+kubectl get deployments -owide --show-labels
+kubectl get rs -owide --show-labels
+kubectl get pods -owide --show-labels
+read -p "Continue?"
 
 kubectl get endpoints alpaca-prod --watch &
 read -p "Continue?"
-kubectl patch deployment alpaca-prod -p "$(cat 07-01_the_service_object_readinessProbe_patch.yaml)"
 
+kubectl patch deployment alpaca-prod -p "$(cat 07-01_the_service_object_readinessProbe_patch.yaml)"
 read -p "Continue?"
+
 ALPACA_POD=$(kubectl get pods -l app=alpaca -o jsonpath='{.items[0].metadata.name}')
 kubectl port-forward $ALPACA_POD 48858:8080
+read -p "Continue?"
--- manifests/07_Service_Discovery/07-02_looking_beyond_the_cluster.sh
+++ manifests/07_Service_Discovery/07-02_looking_beyond_the_cluster.sh
@@ -4,4 +4,6 @@ set -x
 
 kubectl patch service alpaca-prod -p "$(cat 07-02_looking_beyond_the_cluster_patch.yaml)"
 read -p "Continue?"
+
 kubectl describe service alpaca-prod
+read -p "Continue?"
